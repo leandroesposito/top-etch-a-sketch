d6ba6b8324e5f85865f48b6e621d8674ce8e2cda
Leandro Esposito <71948697+leandroesposito@users.noreply.github.com>
2025-01-05 16:17:32 -0300

    Add generate grid functionality
    - Generate grid by creating tiles divs and replace inner content
    - Give new grid size using section min dimention
    - Update tiles width css property

--- script.js
+++ script.js
@@ -1,11 +1,53 @@
 const gridContainer = document.querySelector(".grid-container");
 const gridSection = document.querySelector(".grid-section");
+function generateGrid(numColumns) {
+    const totalTiles = numColumns * numColumns;
+    const tilesFragment = generateTilesFragment(totalTiles);
 
-gridSectionHeigth = gridSection.clientHeight;
-gridSectionWidth = gridSection.clientWidth;
-gridSectionSize = Math.min(gridSectionHeigth, gridSectionWidth);
+    setSquareElementSize(gridContainer, getMinDimensionElement(gridSection));
+    setTileWidthCssProperty(numColumns);
+    replaceElementContent(gridContainer, tilesFragment)
+}
 
-gridContainer.style.width = gridSectionSize + "px";
-gridContainer.style.height = gridSectionSize + "px";
+function generateTilesFragment(numTiles) {
+    const fragment = new DocumentFragment();
+    for (let i = 0; i < numTiles; i++) {
+        const tileElement = generateTileElement();
+        fragment.appendChild(tileElement);
+    }
 
-document.documentElement.style.setProperty("--tile-width-percentage", `${45}%`);
+    return fragment;
+}
+
+function generateTileElement() {
+    const tileElement = document.createElement("div");
+    tileElement.classList.add("tile");
+    //ADD EVENT LISTENER
+
+    return tileElement;
+}
+
+function replaceElementContent(element, newContent) {
+    element.innerHTML = "";
+    element.appendChild(newContent);
+}
+
+function setSquareElementSize(element, width) {
+    element.style.width = width + "px";
+    element.style.height = width + "px";
+}
+
+function getMinDimensionElement(element) {
+    let height = element.clientHeight;
+    let width = element.clientWidth;
+
+    return Math.min(height, width);
+}
+
+function setTileWidthCssProperty(numColumns) {
+    document.documentElement.style.setProperty("--tile-width-percentage", `${(100 / numColumns) - 1}%`);
+}
+
+
+generateGrid(20);
+
